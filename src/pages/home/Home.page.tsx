7075d7df3cfec67ad2e4cd6a7ccb13edfc15d6cd
goh0622 <122134041+goh0622@users.noreply.github.com>
2023-10-25 00:27:40 +0800

    [MBHS-117] job service frontend

--- src/pages/home/Home.page.tsx
+++ src/pages/home/Home.page.tsx
@@ -1,5 +1,8 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import noLeaveImg from 'assets/no-leaves-booked.png';
+import { getJobList } from 'services/job.service';
+import IJob from 'types/job.type';
+
 const authApiEndPoint = import.meta.env.VITE_AUTH_API_END_POINT;
 const jobApiEndPoint = import.meta.env.VITE_JOB_API_END_POINT;
 const Home: React.FC = () => {
@@ -9,6 +12,39 @@ const Home: React.FC = () => {
       // const currentUser = getCurrentUser();
    });
 
+   const [jobs, setJobs] = useState<IJob[]>([]);
+
+   useEffect(() => {
+      const fetchJobs = async () => {
+         getJobList().then((response) => {
+            const jobs = response as { data: IJob };
+            console.log(jobs);
+            if (Array.isArray(jobs.data)) {
+               setJobs(jobs.data);
+            }
+         });
+      };
+      fetchJobs();
+   }, []);
+
+   const renderJobs = () => {
+      return (
+         <ul className="mt-4 mb-4 flex gap-1 overflow-auto md:flex-wrap md:overflow-hidden">
+            {jobs.map((job) => (
+               <li key={job.id} className="shrink-0 md:shrink">
+                  <a href={'/jobs/' + job.id}>
+                     <span className="inline-flex items-center gap-1.5 rounded-lg border-2 px-3 py-1.5 border-indigo-900 text-white bg-indigo-900">
+                        <span aria-hidden="true" role="img" className="text-sm">
+                           {job.title}
+                        </span>
+                     </span>
+                  </a>
+               </li>
+            ))}
+         </ul>
+      );
+   };
+
    return (
       <>
          <div className="card">
@@ -16,12 +52,17 @@ const Home: React.FC = () => {
                <span className="text-[18px]">Find your dream job </span>
             </div>
             <div className="border-t border-gray-350 py-4">
-               <header className="flex flex-col items-center">
-                  <div className="mb-1 text-gray-800 text-lg">There is nothing to show :(</div>
-                  <div className="mt-8">
-                     <img src={noLeaveImg} className="w-auto h-24" />
-                  </div>
-               </header>
+               {renderJobs() ? (
+                  renderJobs()
+               ) : (
+                  <header className="flex flex-col items-center">
+                     <div className="mb-1 text-gray-800 text-lg">There is nothing to show :(</div>
+                     <div className="mt-8">
+                        <img src={noLeaveImg} className="w-auto h-24" />
+                     </div>
+                  </header>
+               )}
+
                <h1>Vite is running in {import.meta.env.MODE}</h1>
                <p>Using AuthService api from {authApiEndPoint}</p>
                <p>Using JobService api from {jobApiEndPoint}</p>
